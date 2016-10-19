9e32124ac0b06c1fff72b6f38b74151394ae5d70
CruzanCaramele <letters2hamza@gmail.com>
2016-10-19 22:34:39 +0800

    make the bastion server only accessible to required cidr range

--- security-groups.tf
+++ security-groups.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "bastion_security" {
 		from_port   = 22
 		to_port     = 22
 		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
+		cidr_blocks = ["61.6.109.231/32"]
 	}
 
   	tags {
