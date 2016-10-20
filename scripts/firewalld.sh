5e561d4bac2cfab2e66f602540e574e866530a3f
CruzanCaramele <letters2hamza@gmail.com>
2016-10-20 23:27:21 +0800

    reload firewalld after blocking ports on default zone

--- scripts/firewalld.sh
+++ scripts/firewalld.sh
@@ -8,11 +8,11 @@ systemctl enable firewalld
 systemctl start  firewalld
 
 # add IP address allowed to ssh into the server
-firewall-cmd --zone-trusted --permanent --add-source=61.6.109.231/32
-firewall-cmd --zone-trusted --add-port=22/tcp
+firewall-cmd --zone=trusted --permanent --add-source=61.6.109.231/32
+firewall-cmd --zone=trusted --add-port=22/tcp
 
 # remove ssh port from the public zone
-firewall-cmd --zone-public --permanent --remove-service=ssh
+firewall-cmd --zone=public --permanent --remove-service=ssh
 
 # reload the firewalld service
 firewall-cmd --reload
