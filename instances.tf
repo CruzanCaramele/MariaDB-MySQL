329263de9b53b5456e760af28d527374d8f1e45e
CruzanCaramele <letters2hamza@gmail.com>
2016-10-21 23:20:03 +0800

    add master and slave servers to same security groups

--- instances.tf
+++ instances.tf
@@ -23,5 +23,44 @@ data "aws_ami" "instance_ami" {
 }
 
 #--------------------------------------------------------------
-# Instance
+# Instances
 #--------------------------------------------------------------
+resource "aws_instance" "database_backup" {
+	ami           		 = "${data.aws_ami.instance_ami.id}"
+	instance_type 	 	 = "t2.micro"
+	subnet_id     		 = "${aws_subnet.private.1.id}"
+	monitoring           = true
+	key_name             = "${aws_key_pair.database_key.key_name}"
+	security_groups      = ["${aws_security_group.database_backup_security_group.id}"]
+	iam_instance_profile = "${aws_iam_instance_profile.backup_instance_profile.id}"
+
+	tags {
+		Name = "backup_backup"
+	}
+}
+
+resource "aws_instance" "database_slave" {
+	ami             = "${data.aws_ami.instance_ami.id}"
+	instance_type   = "t2.micro"
+	subnet_id       = "${aws_subnet.private.2.id}"
+	monitoring      = true
+	key_name        = "${aws_key_pair.database_key.key_name}"
+	security_groups = ["${aws_security_group.database_master_security_group.id}"] 
+
+	tags {
+		Name = "database_slave"
+	}
+}
+
+resource "aws_instance" "database_master" {
+	ami             = "${data.aws_ami.instance_ami.id}"
+	instance_type   = "t2.micro"
+	subnet_id       = "${aws_subnet.private.2.id}"
+	monitoring      = true
+	key_name        = "${aws_key_pair.database_key.key_name}"
+	security_groups = ["${aws_security_group.database_master_security_group.id}"] 
+
+	tags {
+		Name = "database_master"
+	}
+}
