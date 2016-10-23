6cc4c94b7daa8da3f25362783676b7102a622898
CruzanCaramele <letters2hamza@gmail.com>
2016-10-23 21:12:31 +0800

    add coutn variables for all subnets

--- vpc.tf
+++ vpc.tf
@@ -28,6 +28,7 @@ resource "aws_subnet" "private" {
 	vpc_id            = "${aws_vpc.database_setup.id}"
 	availability_zone = "${element(split(",", var.azs), count.index)}"
 	cidr_block        = "${element(split(",", var.private_cidrs), count.index)}"
+	count             = "${length(compact(split(",", var.private_cidrs)))}"
 }
 
 resource "aws_route_table" "private_route_table" {
@@ -39,17 +40,13 @@ resource "aws_route_table" "private_route_table" {
 	}
 
 	tags {
-		Name "private_route_table"
+		Name = "private_route_table"
 	}
 }
 
 resource "aws_route_table_association" "private_route_table_association" {
 	subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
 	route_table_id = "${aws_route_table.private_route_table.id}"
-
-	tags {
-		Name = "private_route_table_association"
-	}
 }
 
 #--------------------------------------------------------------
@@ -59,6 +56,7 @@ resource "aws_subnet" "public" {
 	vpc_id            		= "${aws_vpc.database_setup.id}"
 	availability_zone 		= "${element(split(",", var.azs), count.index)}"
 	cidr_block        		= "${element(split(",", var.public_cidrs), count.index)}"
+	count                   = "${length(compact(split(",", var.public_cidrs)))}"
 	map_public_ip_on_launch = true
 }
 
@@ -76,10 +74,6 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table_association" "public_route_table_association" {
-	subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+	subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
 	route_table_id = "${aws_route_table.public_route_table.id}"
-
-	tags {
-		Name = "public_route_table_association"
-	}
 }
