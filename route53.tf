fb12355215f7f8e1ab3a7e43df22fae635c87ef7
CruzanCaramele <letters2hamza@gmail.com>
2016-10-23 16:52:07 +0800

    Add internal A records for all db servers

---
+++ route53.tf
@@ -0,0 +1,35 @@
+#--------------------------------------------------------------
+# Route53 Hosted Zone
+#--------------------------------------------------------------
+resource "aws_route53_zone" "hosted_zone" {
+	name   = "prod.internal"
+	vpc_id = "${aws_vpc.database_setup.id}"
+
+	tags {
+		Name = "production-zone"
+	}
+}
+
+#--------------------------------------------------------------
+# Route53 Records
+#--------------------------------------------------------------
+resource "aws_route53_record" "backup_instance_record" {
+	zoned_id = "${aws_route53_zone.hosted_zone.id}"
+	name     = "backup.prod.internal"
+	type     = "A"
+	records  = ["aws_instance.database_backup.private_ip"]
+}
+
+resource "aws_route53_record" "slave_instance_record" {
+	zoned_id = "${aws_route53_zone.hosted_zone.id}"
+	name     = "slave.prod.internal"
+	type     = "A"
+	records  = ["aws_instance.database_slave.private_ip"]
+}
+
+resource "aws_route53_record" "master_instance_record" {
+	zoned_id = "${aws_route53_zone.hosted_zone.id}"
+	name     = "master.prod.internal"
+	type     = "A"
+	records  = ["aws_instance.database_master.private_ip"]
+}
