033f69b557fa323c163fb5cd4c0208ea7a6d8f0d
CruzanCaramele <letters2hamza@gmail.com>
2016-10-23 16:51:44 +0800

    add NAT instance security group

--- security-groups.tf
+++ security-groups.tf
@@ -88,3 +88,40 @@ resource "aws_security_group" "database_master_security_group" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 }
+
+#--------------------------------------------------------------
+# NAT Instance Security Group
+#--------------------------------------------------------------
+resource "aws_security_group" "nat_instance_security_group" {
+	name        = "nat_instance_security_group"
+	description = "Security group for NAT instance"
+	vpc_id      = "${aws_vpc.database_setup.id}"
+
+	ingress {
+		from_port       = 22
+		to_port         = 22
+		protocol        = "tcp"
+		security_groups = ["${aws_security_group.bastion_security.id}"]
+	}
+
+	ingress {
+		from_port       = 80
+		to_port         = 80
+		protocol        = "tcp"
+		cidr_blocks     = ["10.0.0.0/24,10.0.1.0/24"]
+	}
+
+	ingress {
+		from_port       = 443
+		to_port         = 443
+		protocol        = "tcp"
+		cidr_blocks     = ["10.0.0.0/24,10.0.1.0/24"]
+	}
+
+	egress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
