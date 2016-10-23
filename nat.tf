9da6a359d154c62b13d21171e1e2f60d62dbb343
CruzanCaramele <letters2hamza@gmail.com>
2016-10-23 21:11:55 +0800

    move nat instance to public subnet

--- nat.tf
+++ nat.tf
@@ -3,16 +3,15 @@
 #--------------------------------------------------------------
 data "aws_ami" "nat_ami" {
 	most_recent      = true
-	executable_users = self
 
 	filter {
 		name    = "owner-alias"
-		valeues = ["amazon"]
+		values = ["amazon"]
 	}
 
 	filter {
 		name    = "name"
-		valeues = ["amzn-ami-vpc-nat*"]
+		values = ["amzn-ami-vpc-nat*"]
 	}
 }
 
@@ -20,12 +19,13 @@ data "aws_ami" "nat_ami" {
 # NAT Instance
 #--------------------------------------------------------------
 resource "aws_instance" "nat_instance" {
-	ami             = "${data.aws_ami.nat_ami.id}"
-	instance_type   = "t2.micro"
-	subnet_id       = "${aws_subnet.public.2.id}"
-	monitoring      = true
-	key_name        = "${aws_key_pair.database_key.key_name}"
-	security_groups = ["${aws_security_group.nat_instance_security_group.id}"]
+	ami               = "${data.aws_ami.nat_ami.id}"
+	instance_type     = "t2.micro"
+	subnet_id         = "${aws_subnet.public.1.id}"
+	monitoring        = true
+	key_name          = "${aws_key_pair.database_key.key_name}"
+	source_dest_check = true
+	security_groups   = ["${aws_security_group.nat_instance_security_group.id}"]
 
 	tags {
 		Name = "NAT"
