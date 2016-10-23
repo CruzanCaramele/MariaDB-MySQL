237dfeac605b7d498b5e3498bcac588ee7061bf0
CruzanCaramele <letters2hamza@gmail.com>
2016-10-23 21:11:37 +0800

    add key-pair

--- key-pair.tf
+++ key-pair.tf
@@ -1,7 +1,7 @@
 #--------------------------------------------------------------
 # Instances' KeyPair "${file(\"ssh_keys/sonar.pub\")}"
 #--------------------------------------------------------------
-resource "aws_key_pair" "database_key_pair" {
+resource "aws_key_pair" "database_key" {
 	key_name   = "database_key"
 	public_key = "${file("ssh_keys/sonar.pub")}"
 }
