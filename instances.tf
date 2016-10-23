e5e5853988c1546cbb43319d26aa8aad3c80cb4d
CruzanCaramele <letters2hamza@gmail.com>
2016-10-23 21:11:24 +0800

    distribute instances into different subnets

--- instances.tf
+++ instances.tf
@@ -14,12 +14,12 @@ data "aws_ami" "instance_ami" {
 		values = ["aw0evgkw8e5c1q413zgy5pjce"]
 	}
 
-	filter {
-		name   = "name"
-		values = ["CentOS-7x86_64"]
-	}
+	# filter {
+	# 	name   = "name"
+	# 	values = ["CentOS-7x86_64"]
+	# }
 
-	owners = ["amazon"]
+	# owners = ["amazon"]
 }
 
 #--------------------------------------------------------------
@@ -28,11 +28,11 @@ data "aws_ami" "instance_ami" {
 resource "aws_instance" "database_backup" {
 	ami           		 = "${data.aws_ami.instance_ami.id}"
 	instance_type 	 	 = "t2.micro"
-	subnet_id     		 = "${aws_subnet.private.1.id}"
+	subnet_id     		 = "${aws_subnet.private.0.id}"
 	monitoring           = true
 	key_name             = "${aws_key_pair.database_key.key_name}"
 	security_groups      = ["${aws_security_group.database_backup_security_group.id}"]
-	iam_instance_profile = "${aws_iam_instance_profile.backup_instance_profile.id}"
+	iam_instance_profile = "${aws_iam_instance_profile.instance_profile.id}"
 
 	tags {
 		Name = "backup_backup"
@@ -42,7 +42,7 @@ resource "aws_instance" "database_backup" {
 resource "aws_instance" "database_slave" {
 	ami             = "${data.aws_ami.instance_ami.id}"
 	instance_type   = "t2.micro"
-	subnet_id       = "${aws_subnet.private.2.id}"
+	subnet_id       = "${aws_subnet.private.1.id}"
 	monitoring      = true
 	key_name        = "${aws_key_pair.database_key.key_name}"
 	security_groups = ["${aws_security_group.database_master_security_group.id}"] 
@@ -55,7 +55,7 @@ resource "aws_instance" "database_slave" {
 resource "aws_instance" "database_master" {
 	ami             = "${data.aws_ami.instance_ami.id}"
 	instance_type   = "t2.micro"
-	subnet_id       = "${aws_subnet.private.2.id}"
+	subnet_id       = "${aws_subnet.private.1.id}"
 	monitoring      = true
 	key_name        = "${aws_key_pair.database_key.key_name}"
 	security_groups = ["${aws_security_group.database_master_security_group.id}"] 
