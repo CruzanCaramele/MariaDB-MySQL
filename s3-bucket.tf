0c30349e5365bf43adddae941ee08a746720bed2
CruzanCaramele <letters2hamza@gmail.com>
2016-10-24 23:38:19 +0800

    make bucket name unique accross AWS

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@
 # S3 Bucket
 #--------------------------------------------------------------
 resource "aws_s3_bucket" "db-backup-bucket" {
-	bucket = "db-backup-bucket"
+	bucket = "dbBucketBacking"
 	acl    = "private"
 
 	tags {
