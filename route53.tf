95334c1b5528749de76ceff676ebf709eb8d061b
CruzanCaramele <letters2hamza@gmail.com>
2016-10-24 23:37:57 +0800

    add internal dns name for db master

--- route53.tf
+++ route53.tf
@@ -17,19 +17,22 @@ resource "aws_route53_record" "backup_instance_record" {
 	zone_id = "${aws_route53_zone.hosted_zone.id}"
 	name     = "backup.prod.internal"
 	type     = "A"
-	records  = ["aws_instance.database_backup.private_ip"]
+	ttl      = "300"
+	records  = ["${aws_instance.database_backup.private_ip}"]
 }
 
 resource "aws_route53_record" "slave_instance_record" {
 	zone_id = "${aws_route53_zone.hosted_zone.id}"
 	name     = "slave.prod.internal"
 	type     = "A"
-	records  = ["aws_instance.database_slave.private_ip"]
+	ttl      = "300"
+	records  = ["${aws_instance.database_slave.private_ip}"]
 }
 
 resource "aws_route53_record" "master_instance_record" {
 	zone_id = "${aws_route53_zone.hosted_zone.id}"
 	name     = "master.prod.internal"
 	type     = "A"
-	records  = ["aws_instance.database_master.private_ip"]
+	ttl      = "300"
+	records  = ["${aws_instance.database_master.private_ip}"]
 }
