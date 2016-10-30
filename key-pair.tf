d941f320b5e77da0b3ecbe4d74a3bfa055563601
CruzanCaramele <letters2hamza@gmail.com>
2016-10-30 21:21:22 +0800

    remove file

--- key-pair.tf
+++
@@ -1,7 +0,0 @@
-#--------------------------------------------------------------
-# Instances' KeyPair "${file(\"ssh_keys/sonar.pub\")}"
-#--------------------------------------------------------------
-resource "aws_key_pair" "database_key" {
-	key_name   = "database_key"
-	public_key = "${file("ssh_keys/database_key.pub")}"
-}
