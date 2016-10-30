b3fa95287f7c19e7f1b75fd4149f3ed79c4a91fb
CruzanCaramele <letters2hamza@gmail.com>
2016-10-30 23:27:45 +0800

    allow ssh connection on different port

--- scripts/firewalld.sh
+++ scripts/firewalld.sh
@@ -4,15 +4,13 @@
 yum install -y firewalld
 
 # enable and start firewalld service
-systemctl enable firewalld
-systemctl start  firewalld
+# systemctl enable firewalld
+# systemctl start  firewalld
 
-# add IP address allowed to ssh into the server
-#firewall-cmd --zone=trusted --permanent --add-source=61.6.109.231/32
-firewall-cmd --zone=trusted --add-port=22/tcp
+# # add IP address allowed to ssh into the server
+# firewall-cmd --zone=trusted --permanent --add-port=22/tcp
 
-# remove ssh port from the public zone
-firewall-cmd --zone=public --permanent --remove-service=ssh
+# # remove ssh port from the public zone
 
-# reload the firewalld service
-firewall-cmd --reload
+# # reload the firewalld service
+# firewall-cmd --reload
