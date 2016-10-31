9d87c2471d8bc2ff262e5090baf18e6841fbaafa
CruzanCaramele <letters2hamza@gmail.com>
2016-10-31 22:22:08 +0800

    add ingress from different azs in public subnets

--- security-groups.tf
+++ security-groups.tf
@@ -48,6 +48,20 @@ resource "aws_security_group" "database_backup_security_group" {
 		cidr_blocks     = ["10.0.2.0/24","10.0.3.0/24","10.0.0.0/24","10.0.1.0/24"]
 	}
 
+	ingress {
+		from_port       = 80
+		to_port         = 80
+		protocol        = "tcp"
+		cidr_blocks     = ["10.0.2.0/24","10.0.3.0/24","10.0.0.0/24","10.0.1.0/24"]
+	}
+
+	ingress {
+		from_port       = 443
+		to_port         = 443
+		protocol        = "tcp"
+		cidr_blocks     = ["10.0.2.0/24","10.0.3.0/24","10.0.0.0/24","10.0.1.0/24"]
+	}
+
 	egress {
 		from_port   = 0
 		to_port     = 0
@@ -78,11 +92,25 @@ resource "aws_security_group" "database_master_security_group" {
 		cidr_blocks     = ["10.0.2.0/24","10.0.3.0/24","10.0.0.0/24","10.0.1.0/24"]
 	}
 
+	ingress {
+		from_port       = 80
+		to_port         = 80
+		protocol        = "tcp"
+		cidr_blocks     = ["10.0.2.0/24","10.0.3.0/24","10.0.0.0/24","10.0.1.0/24"]
+	}
+
+	ingress {
+		from_port       = 443
+		to_port         = 443
+		protocol        = "tcp"
+		cidr_blocks     = ["10.0.2.0/24","10.0.3.0/24","10.0.0.0/24","10.0.1.0/24"]
+	}
+
 	egress {
-		from_port   = 0
-		to_port     = 0
-		protocol    = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
+		from_port       = 0
+		to_port         = 0
+		protocol        = "-1"
+		cidr_blocks     = ["0.0.0.0/0"]
 	}
 }
 
@@ -105,20 +133,109 @@ resource "aws_security_group" "nat_instance_security_group" {
 		from_port       = 80
 		to_port         = 80
 		protocol        = "tcp"
-		cidr_blocks     = ["10.0.0.0/24","10.0.1.0/24"]
+		cidr_blocks     = ["10.0.2.0/24","10.0.3.0/24","10.0.0.0/24","10.0.1.0/24"]
 	}
 
 	ingress {
 		from_port       = 443
 		to_port         = 443
 		protocol        = "tcp"
-		cidr_blocks     = ["10.0.0.0/24","10.0.1.0/24"]
+		cidr_blocks     = ["10.0.2.0/24","10.0.3.0/24","10.0.0.0/24","10.0.1.0/24"]
 	}
 
 	egress {
-		from_port   = 0
-		to_port     = 0
-		protocol    = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
+		from_port   	= 0
+		to_port     	= 0
+		protocol    	= "-1"
+		cidr_blocks 	= ["0.0.0.0/0"]
 	}
+}
+
+#--------------------------------------------------------------
+# Consul Security Group
+#--------------------------------------------------------------
+resource "aws_security_group" "consul_security_group" {
+    name = "consul_security_group"
+    description = "Security Group for consul"
+    vpc_id = "${aws_vpc.database_setup.id}"
+
+    // allows traffic from the SG itself for tcp
+    ingress {
+        from_port = 0
+        to_port   = 65535
+        protocol  = "tcp"
+        self      = true
+    }
+
+    // allows traffic from the SG itself for udp
+    ingress {
+        from_port = 0
+        to_port   = 65535
+        protocol  = "udp"
+        self 	  = true
+    }
+
+    // allow traffic for TCP 22 (SSH)
+    ingress {
+        from_port 	= 22
+        to_port 	= 22
+        protocol 	= "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // allow traffic for TCP 8300 (Server RPC)
+    ingress {
+        from_port 	= 8300
+        to_port 	= 8300
+        protocol 	= "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // allow traffic for TCP 8301 (Serf LAN)
+    ingress {
+        from_port 	= 8301
+        to_port 	= 8301
+        protocol 	= "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // allow traffic for UDP 8301 (Serf LAN)
+    ingress {
+        from_port   = 8301
+        to_port     = 8301
+        protocol    = "udp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // allow traffic for TCP 8400 (Consul RPC)
+    ingress {
+        from_port   = 8400
+        to_port     = 8400
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // allow traffic for TCP 8500 (Consul Web UI)
+    ingress {
+        from_port   = 8500
+        to_port     = 8500
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // allow traffic for TCP 8600 (Consul DNS Interface)
+    ingress {
+        from_port   = 8600
+        to_port     = 8600
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // allow traffic for UDP 8600 (Consul DNS Interface)
+    ingress {
+        from_port   = 8600
+        to_port     = 8600
+        protocol    = "udp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
