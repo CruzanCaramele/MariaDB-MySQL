4e0ac6a7c69c2e7cdd07e13e44d094fb2ad3f184
CruzanCaramele <letters2hamza@gmail.com>
2016-11-01 23:12:45 +0800

    allow ssh only through the trusted zone

--- scripts/firewalld.sh
+++ scripts/firewalld.sh
@@ -4,13 +4,15 @@
 yum install -y firewalld
 
 # enable and start firewalld service
-# systemctl enable firewalld
-# systemctl start  firewalld
+systemctl enable firewalld
+systemctl start  firewalld
 
-# # add IP address allowed to ssh into the server
-# firewall-cmd --zone=trusted --permanent --add-port=22/tcp
+# add IP address allowed to ssh into the server
+firewall-cmd --zone=trusted --permanent --add-source=0.0.0.0/0
+firewall-cmd --zone=trusted --permanent --add-port=22/tcp
 
-# # remove ssh port from the public zone
+# remove ssh port from the public zone
+firewall-cmd --zone=public --permanent --remove-service=ssh
 
-# # reload the firewalld service
-# firewall-cmd --reload
+# reload the firewalld service
+firewall-cmd --reload
