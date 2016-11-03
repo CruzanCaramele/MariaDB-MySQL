5883e59cd9ff281937d4fc958e0ef7de8c27e0e7
CruzanCaramele <letters2hamza@gmail.com>
2016-11-04 00:04:18 +0800

    remove redundant rules on primary group

--- security-groups.tf
+++ security-groups.tf
@@ -22,47 +22,6 @@ resource "aws_security_group" "primary" {
         self      = true
     }
 
-    // allow traffic for TCP 22
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["${var.source_cidr_block}"]
-    }
-
-    // allow traffic for TCP 80
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["${var.source_cidr_block}"]
-    }
-
-    // allow traffic for TCP 443
-    ingress {
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["${var.source_cidr_block}"]
-    }
-
-
-    // allow traffic for TCP 1099 (JMX)
-    ingress {
-        from_port   = 1099
-        to_port     = 1099
-        protocol    = "tcp"
-        cidr_blocks = ["${var.source_cidr_block}"]
-    }
-
-    // allow traffic for TCP 8080
-    ingress {
-        from_port   = 8080
-        to_port     = 8080
-        protocol    = "tcp"
-        cidr_blocks = ["${var.source_cidr_block}"]
-    }
-
     egress {
         from_port   = 0
         to_port     = 0
