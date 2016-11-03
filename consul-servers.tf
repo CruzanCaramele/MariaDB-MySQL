a4a8ade8edf3c6e5b0264ed3488fd0c09fc132fa
CruzanCaramele <letters2hamza@gmail.com>
2016-11-04 00:01:48 +0800

    remove nat instance and use nat gateway

--- consul-servers.tf
+++ consul-servers.tf
@@ -12,19 +12,18 @@ data "atlas_artifact" "Consul" {
 #--------------------------------------------------------------
 resource "aws_instance" "consul_server" {
 	ami             			= "${data.atlas_artifact.Consul.metadata_full.region-us-east-1}"
-	count                       = 3
 	instance_type   			= "t2.micro"
-	subnet_id       			= "${element(aws_subnet.private_subnet.*.id, count.index)}"
+	subnet_id       			= "${aws_subnet.private.0.id}"
 	security_groups 			= ["${aws_security_group.consul_security_group.id}",
 	                               "${aws_security_group.primary.id}"]
 	key_name        			= "${module.ssh_keys.key_name}"
 	depends_on                  = ["aws_internet_gateway.database_gateway"]
 	monitoring      			= true
-	associate_public_ip_address = true
+	private_ip                  = "10.0.0.95"
 
 	tags {
-		Name = "bastion_host"
-		role = "bastion"
+		Name = "consul_server"
+		role = "consul_leader"
 	}
 
 	connection {
