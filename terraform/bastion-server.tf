862e973d61865ee7633491af7aa435e62b33c03e
CruzanCaramele <letters2hamza@gmail.com>
2016-11-05 19:34:15 +0800

    use only one public subnet as one bastion host is needed

--- terraform/bastion-server.tf
+++ terraform/bastion-server.tf
@@ -2,33 +2,36 @@
 # Bastion Artifact (AMI)
 #-----------------------------------------1---------------------
 data "atlas_artifact" "Bastion" {
-	name    = "Panda/Bastion"
-	type    = "amazon.image"
-	version = "latest"
+  name    = "Panda/Bastion"
+  type    = "amazon.image"
+  version = "latest"
 }
 
 #--------------------------------------------------------------
 # Bastion Instance
 #--------------------------------------------------------------
 resource "aws_instance" "bastion_server" {
-	ami             			= "${data.atlas_artifact.Bastion.metadata_full.region-us-east-1}"
-	instance_type   			= "t2.micro"
-	subnet_id       			= "${aws_subnet.public.1.id}"
-	security_groups 			= ["${aws_security_group.bastion_security.id}",
-	                               "${aws_security_group.consul_security_group.id}",
-	                               "${aws_security_group.primary.id}"]
-	key_name        			= "${module.ssh_keys.key_name}"
-	depends_on                  = ["aws_internet_gateway.database_gateway","aws_instance.consul_server"]
-	monitoring      			= true
-	associate_public_ip_address = true
+  ami           = "${data.atlas_artifact.Bastion.metadata_full.region-us-east-1}"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.public.id}"
 
-	tags {
-		Name = "bastion_host"
-		role = "bastion"
-	}
+  security_groups = ["${aws_security_group.bastion_security.id}",
+    "${aws_security_group.consul_security_group.id}",
+    "${aws_security_group.primary.id}",
+  ]
 
-	connection {
-		user        = "root"
-		private_key = "${module.ssh_keys.private_key_path}"
-	}
+  key_name                    = "${module.ssh_keys.key_name}"
+  depends_on                  = ["aws_internet_gateway.database_gateway", "aws_instance.consul_server"]
+  monitoring                  = true
+  associate_public_ip_address = true
+
+  tags {
+    Name = "bastion_host"
+    role = "bastion"
+  }
+
+  connection {
+    user        = "root"
+    private_key = "${module.ssh_keys.private_key_path}"
+  }
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,98 +2,97 @@
 # Bastion Artifact (AMI)
 #--------------------------------------------------------------
 resource "aws_vpc" "database_setup" {
-	cidr_block           = "10.0.0.0/16"
-	enable_dns_support   = true
-	enable_dns_hostnames = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-	tags {
-		Name = "database_setup"
-	}
+  tags {
+    Name = "database_setup"
+  }
 }
 
 #--------------------------------------------------------------
 # Internet Gateway
 #--------------------------------------------------------------
 resource "aws_internet_gateway" "database_gateway" {
-	vpc_id = "${aws_vpc.database_setup.id}"
+  vpc_id = "${aws_vpc.database_setup.id}"
 
-	tags {
-		Name = "database_internet_gateway"
-	}
+  tags {
+    Name = "database_internet_gateway"
+  }
 }
+
 #--------------------------------------------------------------
 # Private subnet
 #--------------------------------------------------------------
 resource "aws_subnet" "private" {
-	vpc_id            = "${aws_vpc.database_setup.id}"
-	availability_zone = "${element(split(",", var.azs), count.index)}"
-	cidr_block        = "${element(split(",", var.private_cidrs), count.index)}"
-	count             = "${length(compact(split(",", var.private_cidrs)))}"
+  vpc_id            = "${aws_vpc.database_setup.id}"
+  availability_zone = "${element(split(",", var.azs), count.index)}"
+  cidr_block        = "${element(split(",", var.private_cidrs), count.index)}"
+  count             = "${length(compact(split(",", var.private_cidrs)))}"
 }
 
 resource "aws_route_table" "private_route_table" {
-	vpc_id = "${aws_vpc.database_setup.id}"
-	count  = "${length(split(",", var.azs))}"
+  vpc_id = "${aws_vpc.database_setup.id}"
+  count  = "${length(split(",", var.azs))}"
 
-	route {
-		cidr_block  = "0.0.0.0/0"
-		nat_gateway_id = "${element(aws_nat_gateway.database_nat.*.id, count.index)}"
-	}
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${element(aws_nat_gateway.database_nat.*.id, count.index)}"
+  }
 
-	tags {
-		Name = "private_route_table"
-	}
+  tags {
+    Name = "private_route_table"
+  }
 }
 
 resource "aws_route_table_association" "private_route_table_association" {
-	count          = "${length(compact(split(",", var.private_cidrs)))}"
-	subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
-	route_table_id = "${element(aws_route_table.private_route_table.*.id, count.index)}"
+  count          = "${length(compact(split(",", var.private_cidrs)))}"
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private_route_table.*.id, count.index)}"
 }
 
 #--------------------------------------------------------------
 # NAT Eip
 #--------------------------------------------------------------
 resource "aws_eip" "database_nat_eip" {
-	vpc   = true
-	count = "${length(split(",", var.azs))}"
+  vpc   = true
+  count = "${length(split(",", var.azs))}"
 }
 
 #--------------------------------------------------------------
 # NAT Gateway
 #--------------------------------------------------------------
 resource "aws_nat_gateway" "database_nat" {
-	count         = "${length(split(",", var.azs))}"
-	allocation_id = "${element(aws_eip.database_nat_eip.*.id, count.index)}"
-	subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
+  count         = "${length(split(",", var.azs))}"
+  allocation_id = "${element(aws_eip.database_nat_eip.*.id, count.index)}"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
 }
 
 #--------------------------------------------------------------
 # Public subnet
 #--------------------------------------------------------------
 resource "aws_subnet" "public" {
-	vpc_id            		= "${aws_vpc.database_setup.id}"
-	availability_zone 		= "${element(split(",", var.azs), count.index)}"
-	cidr_block        		= "${element(split(",", var.public_cidrs), count.index)}"
-	count                   = "${length(compact(split(",", var.public_cidrs)))}"
-	map_public_ip_on_launch = true
+  vpc_id                  = "${aws_vpc.database_setup.id}"
+  availability_zone       = "us-east-1a"
+  cidr_block              = "${var.public_cidrs}"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_route_table" "public_route_table" {
-	vpc_id = "${aws_vpc.database_setup.id}"
+  vpc_id = "${aws_vpc.database_setup.id}"
 
-	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_internet_gateway.database_gateway.id}"
-	}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.database_gateway.id}"
+  }
 
-	tags {
-		Name = "public_route_table"
-	}
+  tags {
+    Name = "public_route_table"
+  }
 }
 
 resource "aws_route_table_association" "public_route_table_association" {
-	count          = "${length(compact(split(",", var.public_cidrs)))}"
-	subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
-	route_table_id = "${aws_route_table.public_route_table.id}"
+  subnet_id      = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
 }
