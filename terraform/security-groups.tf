372b524c708b740866e75171d6db03a464204241
CruzanCaramele <letters2hamza@gmail.com>
2016-11-05 09:54:52 +0800

    add egress rule for instances

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -207,4 +207,11 @@ resource "aws_security_group" "consul_security_group" {
         protocol    = "udp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
